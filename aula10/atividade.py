8aad8fc0888cea4bc8a131b3fc3176b04417e316
luisaabarros <luisaabarros1707@gmail.com>
2025-12-16 21:38:46 -0300

    Sobe atividade 1

--- aula10/atividade.py
+++ aula10/atividade.py
@@ -1,2 +1,20 @@
 # ATIVIDADE 01
 #VSCode - GitHub
+def soma_valores(v1, v2):
+    t = v1 + v2
+    return t
+
+for n in range(3):
+    try:
+        valor1 = int(input('Informe o 1º valor: '))
+        valor2 = int(input('Informe o 2º valor: '))
+        total = soma_valores(valor1, valor2)
+    except ValueError:
+        print('Erro! Apenas numeros.')
+    except KeyboardInterrupt:
+        print('\nOperação encerrada pelo usuário.')
+    else:
+        print(f'O resultado da soma dos números é {total}!')
+    finally:
+        print('Operação finalizada.')
+    
